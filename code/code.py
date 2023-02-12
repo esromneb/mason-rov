8e7bc442169b3356b791f670ba51fdc6e3001353
Benjamin Morse <bjmorse@gmail.com>
2023-02-11 19:52:47 -0800

    code for 4 inputs

--- code/code.py
+++ code/code.py
@@ -5,20 +5,42 @@ import time
 led = digitalio.DigitalInOut(board.D13)
 led.direction = digitalio.Direction.OUTPUT
 
-lr = digitalio.DigitalInOut(board.D10)
+lr = digitalio.DigitalInOut(board.A5)
 lr.direction = digitalio.Direction.INPUT
 
-runs = 0
+
+i0 = digitalio.DigitalInOut(board.D5)
+i0.direction = digitalio.Direction.INPUT
+
+i1 = digitalio.DigitalInOut(board.D6)
+i1.direction = digitalio.Direction.INPUT
+
+i2 = digitalio.DigitalInOut(board.D9)
+i2.direction = digitalio.Direction.INPUT
+
+i3 = digitalio.DigitalInOut(board.D10)
+i3.direction = digitalio.Direction.INPUT
+
+
+
 while True:
-    
-    led.value = True
-    time.sleep(0.000000001)
-    led.value = False
-    time.sleep(0.000000001)
-    
-    # print(runs)
-    # runs += 1
-    if(lr.value):
-        print("HIGH")
-    else:
-        print("LOW")
+    #print("button: {}".format(lr.value))
+    print("i0: {} i1: {} i2: {} i3 {}".format(int(i0.value), int(i1.value), int(i2.value), int(i3.value)))
+    # print(lr.value)
+    time.sleep(0.001)
+
+
+# runs = 0
+# while True:
+
+#     led.value = True
+#     time.sleep(0.000000001)
+#     led.value = False
+#     time.sleep(0.000000001)
+
+#     # print(runs)
+#     # runs += 1
+#     if(lr.value):
+#         print("HIGH")
+#     else:
+#         print("LOW")
